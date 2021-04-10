9d2fde9841bec1723c2b28586065a454c08f176f
Matthew Becker <becker@tamu.edu>
2021-04-10 18:52:59 -0500

    Rename viewer to imageWindow

---
+++ frontend/src/components/imageWindow.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ImageWindow extends Component {
+    state = {  }
+    render() { 
+        return ( <div>ImageWindow component</div> );
+    }
+}
+ 
+export default ImageWindow;
