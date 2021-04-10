95a58666e4ac842e143eb858cced1077c2992c5a
Matthew Becker <becker@tamu.edu>
2021-04-10 18:53:24 -0500

    History --> HistorySidebar & HistoryLine

---
+++ frontend/src/components/historyLine.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class HistoryLine extends Component {
+    state = {  }
+    render() { 
+        return ( <div>HistoryLine component</div> );
+    }
+}
+ 
+export default HistoryLine;
---
+++ frontend/src/components/historySidebar.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class HistorySidebar extends Component {
+    state = {  }
+    render() { 
+        return ( <div>HistorySidebar component</div> );
+    }
+}
+ 
+export default HistorySidebar;
