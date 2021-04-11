c69c18aaa92b16c04cdb4bf152b2f85f83ce9aa4
Matthew Becker <becker@tamu.edu>
2021-04-11 02:54:21 -0500

    Implement AUTH token passthrough
    App.js now passes the auth token retrieved in login.jsx to imageWindow.jsx in props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,12 +11,20 @@ import Login from './components/login'
 // Make history panel and login sidebar collapsable
 
 class App extends Component {
+  state = {
+    token: ""
+  }
+
+  receiveToken(rectoken) {
+    this.setState({token: rectoken})
+  }
+
   render(){
     return (
       <div className="d-flex flex-row justify-content-between ">
         <div className="p-0"><HistorySidebar /></div>
-        <div className="p-5"><ImageWindow /></div>
-        <div className="p-0"><Login /></div>
+        <div className="p-5"><ImageWindow token={this.state.token}/></div>
+        <div className="p-0"><Login sendToken={this.receiveToken.bind(this)}/></div>
       </div>
     );
   }
