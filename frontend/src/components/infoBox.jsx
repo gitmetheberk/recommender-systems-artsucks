8f53d87938804a3317e8ebae9d1362d0815e2373
root <becker@tamu.edu>
2021-04-15 15:21:17 +0000

    Update InfoBox to point to frontend datasets.html

--- frontend/src/components/infoBox.jsx
+++ frontend/src/components/infoBox.jsx
@@ -9,7 +9,7 @@ class InfoBox extends Component {
         return ( 
             <div style={{width: '105px', height: '100px'}} className="d-flex flex-column justify-content-center align-items-center rectangle bg-info rounded-left">
                     <a style={{color: "#5cc41f"}} className="h4 p-0" href="https://github.tamu.edu/jared-pauletti/art-recommender">Github</a>
-                    <a style={{color: "#50d9ae"}} className="h4 p-0" href={this.props.requestUrl + "static/datasets.html"}>Datasets</a>
+                    <a style={{color: "#50d9ae"}} className="h4 p-0" href="datasets.html">Datasets</a>
             </div>
         );
     }
