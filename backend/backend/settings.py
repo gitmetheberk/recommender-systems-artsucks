77af42504ef68be66c4933dcd50c8b7589c986c6
root <becker@tamu.edu>
2021-04-17 20:15:14 +0000

    Migrate from sqlite3 to MySQL
    The entire application now runs using a MySQL database instead
    of the sqlite3 database. The sqlite3 database did not have
    all support needed to support the art feature arrays.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'se_i6rL0__S&Ugich2ge',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
