0e020b4514cfaac22c11af18635f4c492ae8637f
root <becker@tamu.edu>
2021-04-22 06:02:28 +0000

    Add report generation for recommender evaluation
    When viewing userprofiles in the admin panel, after selecting one or
    more profiles a report can be generated from the drop down menu at the
    top. This report theoretically evaluates the recommender performance
    using a somewhat reliable metric... so far, results show either the
    metrics are wrong or the recommender simply doesn't work :-(

--- backend/artrec/admin.py
+++ backend/artrec/admin.py
@@ -1,4 +1,6 @@
+# Django imports
 from django.contrib import admin
+from django.shortcuts import redirect
 
 # Import the models
 from .models import Artwork
@@ -7,6 +9,8 @@ from .models import HistoryLine
 
 # Other imports
 import time
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 # Define list views for the models
 class ArtworkAdmin(admin.ModelAdmin):
@@ -15,8 +19,6 @@ class ArtworkAdmin(admin.ModelAdmin):
 
 # Used to generate reports for the userprofiles in the queryset
 def UserProfile_generateReports(modeladmin, request, queryset):
-    print("I generated a report (believe me (it happened (definitely)))")
-
     # Open the report file for writing
     filepath = "static/reports/" + "report_" + time.strftime("%Y%m%d-%H%M%S") + ".txt"
     file = open(filepath, 'x')
@@ -26,23 +28,76 @@ def UserProfile_generateReports(modeladmin, request, queryset):
         file.write("BEGIN REPORT FOR USER: {}\n".format(profile.user.username))
 
         # Get all historyLine information for the user
-        history = HistoryLine.objects.filter(user=profile)
+        history = HistoryLine.objects.filter(user=profile).order_by('updated')
         history_count = history.count()
         history_likes = HistoryLine.objects.filter(user=profile, status='L').count()
         history_dislikes = HistoryLine.objects.filter(user=profile, status='D').count()
         file.write("Total HistoryLines: {}\nTotal Likes:        {}\nTotal Dislikes:     {}\n\n"
                   .format(history_count, history_likes, history_dislikes))
 
-        # TODO: Some complex stuff needs to happen here to implement the metrics
+        # If this user doesn't have enough likes/total ratings skip
+        if history_count < 25 or history_likes < 15:
+            file.write("Further caluclations aborted due to low interactions")
+            continue
+
+        # Load all user history into a list for ease of use where 1 = L and 0 = D
+        histArr = []
+        processed = False
+        likeCount = 0
+        for line in history:
+            # Process out the randomly generated interactions from the beginning of the user's history
+            if not processed:
+                if line.status == 'L':
+                    likeCount += 1
+                    if likeCount == 14:
+                        processed = True
+                
+            # Processing complete, proceed normally
+            else:
+                histArr.append(1 if line.status == 'L' else 0)
+        
+        # Print updated information following removal of random items
+        file.write("Recommended history: {}\nTotal Likes:         {}\nTotal Dislikes:      {}\n\n"
+                  .format(len(histArr), np.sum(histArr), len(histArr) - np.sum(histArr)))
+
+        # List of splits to use for metrics (In percent to evaluate vs. history)
+        splits = (0.5, 0.33, 0.25)
+        for split in splits:
+            # Split the history into a recent piece and a new piece
+            hist, new = train_test_split(histArr, shuffle=False, test_size=split)
+            
+            # DEBUGGING
+            # print(hist,end="")
+            # print(new)
+            # print(histArr)
+            # print("you got lucky")
 
 
+            # Sum likes against total
+            hist_likes = np.sum(hist)
+            new_likes = np.sum(new)
 
+            # Calculate the ratios
+            hist_ratio = hist_likes / len(hist)
+            new_ratio = new_likes / len(new)
+            percentChange = ((new_ratio / hist_ratio) - 1) * 100
+
+            # Write the results (in a very inefficient manner since the multi-line string didn't want to cooperate)
+            file.write("Evaluating performance of most recent interactions ({}/{})\n".format(round(split*100), 100-round(split*100)))
+            file.write("    Historical data | Newest Data\n")
+            file.write("Num. Likes: {0:<5}   | {1:<5}  \n".format(hist_likes, new_likes))
+            file.write("% Liked:    {0:<5} % | {1:<5} %\n".format(round(hist_ratio * 100, 2),round(new_ratio * 100, 2)))
+            file.write("Change in newer data: {} %\n".format(round(percentChange, 2)))
+            file.write("STATUS: {}\n\n".format("IMPROVED" if percentChange > 0 else ("WORSE" if percentChange < 0 else "NO CHANGE")))
 
         file.write("-----------------------\n")
 
     print("REPORT GENERATED: {}".format(filepath))
     print("It can be accessed at 104.236.113.146:8000/{}".format(filepath))
     file.close()
+
+    # Direct to the new report
+    return redirect('/{}'.format(filepath))
 
 UserProfile_generateReports.short_description = "Generate report"
 
